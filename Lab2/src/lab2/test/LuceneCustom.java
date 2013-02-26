e915d3dd6cd9f68b71fbb8a987976cdad9f30577
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-27 00:53:15 +0100

    do the associated refactoring

--- Lab2/src/lab2/test/LuceneCustom.java
+++ Lab2/src/lab2/test/LuceneCustom.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab2.indexing;
+package lab2.test;
 
 import java.io.BufferedReader;
 import java.io.File;
