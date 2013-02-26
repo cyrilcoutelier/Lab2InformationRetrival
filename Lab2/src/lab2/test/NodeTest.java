f85705ec59535b637c12a9203bf2e468417e7f7e
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-27 00:58:58 +0100

    move node test to a dedicated file

---
+++ Lab2/src/lab2/test/NodeTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import lab2.document.WordCount;
+
+/**
+ *
+ * @author Cyril
+ */
+public class NodeTest {
+      public static void main(String[] args) {
+       // System.out.println("the value of a is : " + (int)'a');
+       // System.out.print((char)(2+'a'));
+        Node node = new Node();
+        node.addWord("test");
+        node.addWord("test");
+        node.addWord("test");
+     ///   node.addWord("test");
+        node.addWord("testb");
+        node.addWord("abc");
+        node.caculateWordCount();
+        ArrayList<WordCount> wordsInfo = node.getWordCounts();
+        
+        Iterator<WordCount> it = wordsInfo.iterator();
+        
+        while (it.hasNext()) {
+            it.next().showInfo();
+        }
+       // node.printInfo();
+        
+    }
+}
