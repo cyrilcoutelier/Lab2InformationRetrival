b4ae7e846be3fc8e07574c0fa62d3f320179e220
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-26 14:50:25 +0100

    implement the HeaderData

---
+++ Lab2/src/lab2/HeaderDataMap.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Cyril
+ */
+public class HeaderDataMap implements HeaderData {
+
+  private Map<String, Integer> termsMap = new HashMap<>();
+  int currentIdx = 0;
+
+  @Override
+  public void tryRegisterTerm(String term) {
+    if (this.notRegistred(term)) {
+      this.registerTerm(term);
+    }
+  }
+
+  private boolean notRegistred(String term) {
+    return !this.termsMap.containsKey(term);
+  }
+
+  private void registerTerm(String term) {
+    this.termsMap.put(term, this.currentIdx);
+    this.currentIdx++;
+  }
+
+  @Override
+  public int getTermIndex(String term) {
+    if (notRegistred(term)) {
+      String msg = "the term " + term + " is not registred";
+      throw new RuntimeException(msg);
+    } else {
+      return this.termsMap.get(term);
+    }
+  }
+}
