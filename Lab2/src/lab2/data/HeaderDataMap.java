57c47fcef1624e2a97e56cbc78d65fbd1c39cd4f
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-26 17:39:01 +0100

    doing the refactoring

--- Lab2/src/lab2/data/HeaderDataMap.java
+++ Lab2/src/lab2/data/HeaderDataMap.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab2;
+package lab2.data;
 
 import java.util.ArrayList;
 import java.util.HashMap;
