8cfef520cf330a465508fcdc0386653fde1f3673
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-26 15:32:32 +0100

    adding constant for Arff strings

---
+++ Lab2/src/lab2/arff/Section.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2.arff;
+
+/**
+ *
+ * @author Cyril
+ */
+public class Section {
+
+  public static final String RELATION = "@RELATION";
+  public static final String ATTRIBUTE = "@ATTRIBUTE";
+  public static final String DATA = "@DATA";
+
+  private Section() {
+  }
+}
