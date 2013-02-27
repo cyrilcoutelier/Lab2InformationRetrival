110a3045a211fd54e865d1be2e68ad48f9505a2d
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-27 01:17:33 +0100

    adding first main elements

--- Lab2/src/lab2/test/Lab2.java
+++ Lab2/src/lab2/test/Lab2.java
@@ -4,6 +4,10 @@
  */
 package lab2.test;
 
+import java.util.ArrayList;
+import lab2.document.DocumentStatistics;
+import lab2.document.WordCount;
+
 /**
  *
  * @author Cyril
@@ -14,6 +18,30 @@ public class Lab2 {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    String usage = "lab2.test.Lab2"
+            + " INDEX_PATH ARFF_PATH\n\n"
+            + "This create an arff file with the given index";
+    String indexPath = "";
+    
+    
+    DocumentStatistics statistics = new DocumentStatistics(indexPath);
+    statistics.classifyFiles();
+    /*    ArrayList<ArrayList<WordCount>> documentStatistics = statistics.getDocumentStatistics();
+     Iterator<ArrayList<WordCount>> docsIt = documentStatistics.iterator();
+     while (docsIt.hasNext()) {
+     Iterator<WordCount> wordIt = docsIt.next().iterator();
+     while (wordIt.hasNext()) {
+     wordIt.next().showInfo();
+     }
+     }*/
+    ArrayList<WordCount> collection = statistics.getCollectionStatistics();
+    /*   Iterator<WordCount> it = collection.iterator();
+     while (it.hasNext()) {
+     it.next().showInfo();
+     }*/
+    
+    
+
     // TODO code application logic here
   }
 }
