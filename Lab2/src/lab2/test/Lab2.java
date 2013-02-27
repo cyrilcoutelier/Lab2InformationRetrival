5aa36d30b7bc71f3012e112110becafe20d7e4b9
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-27 01:39:58 +0100

    remove unused code and comments

--- Lab2/src/lab2/test/Lab2.java
+++ Lab2/src/lab2/test/Lab2.java
@@ -27,40 +27,20 @@ public class Lab2 {
             + "This create an arff file with the given index";
     String indexPath = args[0];
     String arffPath = args[1];
-    
+
     PrintStream ps = new PrintStream(arffPath);
-    
+
     DocumentStatistics statistics = new DocumentStatistics(indexPath);
     statistics.classifyFiles();
-    
-    
-    
-    /*    ArrayList<ArrayList<WordCount>> documentStatistics = statistics.getDocumentStatistics();
-     Iterator<ArrayList<WordCount>> docsIt = documentStatistics.iterator();
-     while (docsIt.hasNext()) {
-     Iterator<WordCount> wordIt = docsIt.next().iterator();
-     while (wordIt.hasNext()) {
-     wordIt.next().showInfo();
-     }
-     }*/
-    ArrayList<WordCount> collection = statistics.getCollectionStatistics();
-    /*   Iterator<WordCount> it = collection.iterator();
-     while (it.hasNext()) {
-     it.next().showInfo();
-     }*/
-    
-    
-
-    // TODO code application logic here
   }
 
   static private HeaderData createHeaderData(ArrayList<WordCount> collectionStatistics) {
     HeaderData headerData = new HeaderDataMap();
-    
+
     for (WordCount wordCount : collectionStatistics) {
       headerData.tryRegisterTerm(wordCount.word);
     }
-    
+
     return headerData;
   }
 }
