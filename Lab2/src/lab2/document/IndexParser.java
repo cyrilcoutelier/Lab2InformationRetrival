18b68f480e48cd85abe32236c3b811481cc35a71
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-28 10:07:22 +0100

    adding code to extract className from path

--- Lab2/src/lab2/document/IndexParser.java
+++ Lab2/src/lab2/document/IndexParser.java
@@ -5,6 +5,8 @@
 package lab2.document;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
 import lab2.data.DocsData;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Terms;
@@ -19,16 +21,26 @@ public class IndexParser {
   IndexReader indexReader;
   DocsData docsData;
   String fieldName;
+  String pathFieldName;
+  Set<String> pathFieldSet;
 
   public IndexParser(IndexReader indexReader, DocsData docsData, String fieldName) {
     this.indexReader = indexReader;
     this.docsData = docsData;
     this.fieldName = fieldName;
+    pathFieldSet = new TreeSet<>();
+    
+    this.pathFieldSet.add(pathFieldName);
   }
 
   public void parse() throws IOException {
     int maxDocs = this.indexReader.maxDoc();
     for (int i = 0; i < maxDocs; ++i) {
+      String path = this.indexReader.document(i, pathFieldSet).get(pathFieldName);
+      String[] pathElems = path.split("'\\'");
+      String className = pathElems[pathElems.length - 1];
+      
+      this.indexReader.document(i, pathFieldSet);
       this.docsData.startDoc();
 
       Terms termVector = this.indexReader.getTermVector(i, fieldName);
